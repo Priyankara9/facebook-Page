79952d407e18853648828ebb5cd555e1c1da054e
Priyankara9 <cemahesh92@gmail.com>
2024-02-18 10:11:42 +0530

    install bootstrap and create login Component

--- facebook/src/App.tsx
+++ facebook/src/App.tsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 import "./App.css";
+import Login from "./assets/component/login";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <Login></Login>
+    </>
+  );
 }
 
 export default App;
