ca77d5fc1129b3972b6d2bde32619c2077e8ab66
Priyankara9 <cemahesh92@gmail.com>
2024-02-18 10:39:38 +0530

    applied css to login form

--- facebook/src/assets/component/login.tsx
+++ facebook/src/assets/component/login.tsx
@@ -1,49 +1,43 @@
 import { useState } from "react";
+import "./login.css";
 
 function Login() {
   return (
     <>
-      <div>
-        <h2>Facebook</h2>
+      <div className="text-center">
+        <h1>Facebook</h1>
       </div>
-      <form>
-        <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+      <div className="container d-flex  justify-content-center justify-items-center lg-size">
+        <form className="m-2 form-size">
+          <h3 className="h3 mb-3 fw-normal text-center">Log in to Facebook</h3>
 
-        <div className="form-floating">
-          <input
-            type="email"
-            className="form-control"
-            id="floatingInput"
-            placeholder="name@example.com"
-          />
-          <label htmlFor="floatingInput">Email address</label>
-        </div>
-        <div className="form-floating">
-          <input
-            type="password"
-            className="form-control"
-            id="floatingPassword"
-            placeholder="Password"
-          />
-          <label htmlFor="floatingPassword">Password</label>
-        </div>
+          <div className="form-floating">
+            <input
+              type="email"
+              className="form-control m-2"
+              id="floatingInput"
+              placeholder="name@example.com"
+            />
+            <label htmlFor="floatingInput">Email address</label>
+          </div>
+          <div className="form-floating m-3">
+            <input
+              type="password"
+              className="form-control"
+              id="floatingPassword"
+              placeholder="Password"
+            />
+            <label htmlFor="floatingPassword">Password</label>
+          </div>
 
-        <div className="form-check text-start my-3">
-          <input
-            className="form-check-input"
-            type="checkbox"
-            value="remember-me"
-            id="flexCheckDefault"
-          />
-          <label className="form-check-label" htmlFor="flexCheckDefault">
-            Remember me
-          </label>
-        </div>
-        <button className="btn btn-primary w-100 py-2" type="submit">
-          Sign in
-        </button>
-        <p className="mt-5 mb-3 text-body-secondary">&copy; 2017–2023</p>
-      </form>
+          <div className="form-check text-start my-3 text-center">
+            <a href="">forgetten password?</a>
+          </div>
+          <button className="btn btn-primary w-100 py-2" type="submit">
+            Sign in
+          </button>
+        </form>
+      </div>
     </>
   );
 }
